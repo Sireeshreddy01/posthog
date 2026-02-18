ec77789376002af836e2fe0cf9f13c6dd23c1b54
Fernando Gomes <fernando.g@posthog.com>
2026-02-18 17:36:12 -0300

    chore: fix ci errors

--- products/workflows/frontend/Workflows/hogflows/registry/triggers/surveys.tsx
+++ products/workflows/frontend/Workflows/hogflows/registry/triggers/surveys.tsx
@@ -12,8 +12,6 @@ import { urls } from 'scenes/urls'
 
 import { SurveyEventName } from '~/types'
 
-import { FEATURE_FLAGS } from 'lib/constants'
-
 import { registerTriggerType } from 'products/workflows/frontend/Workflows/hogflows/registry/triggers/triggerTypeRegistry'
 import { workflowLogic } from 'products/workflows/frontend/Workflows/workflowLogic'
 
@@ -275,7 +273,6 @@ registerTriggerType({
     label: 'Survey response',
     icon: <IconMessage />,
     description: 'Trigger when a user submits a survey response',
-    featureFlag: FEATURE_FLAGS.WORKFLOWS_SURVEY_TRIGGERS,
     matchConfig: (config) => isSurveyTriggerConfig(config),
     buildConfig: () => ({
         type: 'event',
